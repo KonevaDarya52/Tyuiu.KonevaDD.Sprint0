bead80b7e909c4246799a7d8a767ddc2da9cbab5
KonevaDD <Tyuiu.KonevaDD@outlook.com>
2024-09-20 12:35:24 +0500

    Task7

---
+++ Tyuiu.KonevaDD.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.KonevaDD.Sprint0.Task7.V0.Lib;
+
+public class DataService
+{
+    public static int[] AdditonArrays(int[] num0ne, int[] numTwo)
+    {
+        int[] resultArray = new int[5];
+        for (var i = 0; i < num0ne.Length; i++)
+        {
+            resultArray[i] = num0ne[i] + numTwo[i];
+
+        }
+        return resultArray;
+    }
+}
